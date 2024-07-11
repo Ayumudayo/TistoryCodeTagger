cba4367037d9be695c5ce5eee1a9dad05e7be8d7
Ayumudayo <129680598+Ayumudayo@users.noreply.github.com>
2024-07-11 17:27:34 +0900

    Remove MathJax related codes

--- CodeTagger.js
+++ CodeTagger.js
@@ -1,13 +1,14 @@
 (() => {
     const config = window.codeWrapperConfig || {
-        startWrapper: "``",
-        endWrapper: "``"
+        startWrapper: "`",
+        endWrapper: "`"
     };
 
     const replaceCodeTags = (node, startWrapper, endWrapper) => {
-        if (node.nodeType === Node.ELEMENT_NODE && 
-            (node.classList.contains('MathJax') || node.classList.contains('MathJax_Preview'))) {
-            return;
+        if (node.nodeType === Node.ELEMENT_NODE) {
+            if (node.classList.contains('MathJax') || node.classList.contains('MathJax_Preview')) {
+                return;
+            }
         }
         if (node.nodeType === Node.TEXT_NODE) {
             const regex = new RegExp(escapeRegExp(startWrapper) + "(.*?)" + escapeRegExp(endWrapper), 'g');
@@ -48,46 +49,8 @@
 
     const startReplacement = () => replaceCodeTags(document.body, config.startWrapper, config.endWrapper);
 
-    const isMathJaxRequired = () => {
-        const scripts = Array.from(document.getElementsByTagName('script'));
-        if (scripts.some(script => script.type === 'math/tex' || script.type === 'math/asciimath')) {
-            return true;
-        }
-        const elements = Array.from(document.body.getElementsByTagName('*'));
-        return elements.some(element => element.innerHTML.includes('$') || element.innerHTML.includes('\\('));
-    };
-
-    const loadMathJax = () => {
-        const script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML";
-
-        script.onload = () => {
-            if (typeof MathJax !== 'undefined') {
-                MathJax.Hub.Config({
-                    tex2jax: {
-                        inlineMath: [['$', '$'], ['\\(', '\\)']],
-                        ignoreDelimiters: [['`', '`']]
-                    }
-                });
-                MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
-                MathJax.Hub.Queue(startReplacement);
-            } else {
-                console.error("MathJax failed to load properly");
-            }
-        };
-
-        script.onerror = () => console.error('Failed to load MathJax script');
-
-        document.head.appendChild(script);
-    };
-
     document.addEventListener("DOMContentLoaded", () => {
         startReplacement();
-
-        if (isMathJaxRequired()) {
-            loadMathJax();
-        }
     });
 
     window.startReplacement = startReplacement;
